6f6806d5aca68b62b736b9ec602906be04038ff0
Robert West <robert.trey.west@gmail.com>
2025-07-20 12:38:09 -0400

    Added accessibility and updated styles

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 document.querySelector('.theme-toggle').addEventListener('click', event => {
-  document.body.classList.toggle('dark-mode');
-
-  document.querySelector('.theme-toggle-icon').src = 'assets/images/icon-moon.svg';
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode');
+    document.querySelector('.theme-toggle-icon').src = 'assets/images/icon-moon.svg';
+    document.querySelector('#theme-toggle-name').textContent = 'Toggle theme to dark mode';
+    document.querySelector('.logo').setAttribute('src', 'assets/images/logo-light.svg');
+  } else {
+    document.body.classList.add('dark-mode');
+    document.querySelector('.theme-toggle-icon').src = 'assets/images/icon-sun.svg';
+    document.querySelector('#theme-toggle-name').textContent = 'Toggle theme to light mode';
+    document.querySelector('.logo').setAttribute('src', 'assets/images/logo-dark.svg');
+  }
 });
+
+document.querySelector('.all-filter-button').setAttribute('aria-current', 'true');
 
 document.querySelector('.all-filter-button').addEventListener('click', () => {
   // Apply styles to filter button
-  document.querySelector('.all-filter-button').classList.add('selected');
-  document.querySelector('.active-filter-button').classList.remove('selected');
-  document.querySelector('.inactive-filter-button').classList.remove('selected');
+  document.querySelector('.all-filter-button').setAttribute('aria-current', 'true');
+  document.querySelector('.active-filter-button').setAttribute('aria-current', 'false');
+  document.querySelector('.inactive-filter-button').setAttribute('aria-current', 'false');
 
   // Filter grid to only include active extensions
   document.querySelectorAll('.extension-card').forEach(card => {
@@ -18,18 +28,18 @@ document.querySelector('.all-filter-button').addEventListener('click', () => {
 
 document.querySelector('.active-filter-button').addEventListener('click', () => {
   // Apply styles to filter button
-  document.querySelector('.all-filter-button').classList.remove('selected');
-  document.querySelector('.active-filter-button').classList.add('selected');
-  document.querySelector('.inactive-filter-button').classList.remove('selected');
+  document.querySelector('.all-filter-button').setAttribute('aria-current', 'false');
+  document.querySelector('.active-filter-button').setAttribute('aria-current', 'true');
+  document.querySelector('.inactive-filter-button').setAttribute('aria-current', 'false');
 
   filterToActiveExtensionsOnly();
 });
 
 document.querySelector('.inactive-filter-button').addEventListener('click', () => {
   // Apply styles to filter button
-  document.querySelector('.all-filter-button').classList.remove('selected');
-  document.querySelector('.active-filter-button').classList.remove('selected');
-  document.querySelector('.inactive-filter-button').classList.add('selected');
+  document.querySelector('.all-filter-button').setAttribute('aria-current', 'false');
+  document.querySelector('.active-filter-button').setAttribute('aria-current', 'false');
+  document.querySelector('.inactive-filter-button').setAttribute('aria-current', 'true');
 
   filterToInactiveExtensionOnly();
 });
@@ -41,9 +51,22 @@ document.querySelectorAll('.remove-button').forEach(button => {
 });
 
 document.querySelectorAll('.active-button').forEach(button => {
+  const card = button.closest('.extension-card');
+
+  if (card.classList.contains('active')) {
+    button.setAttribute('aria-pressed', 'true');
+  } else {
+    button.setAttribute('aria-pressed', 'false');
+  }
+
   button.addEventListener('click', () => {
-    const card = button.closest('.extension-card');
     card.classList.toggle('active');
+
+    if (button.getAttribute('aria-pressed') === 'true') {
+      button.setAttribute('aria-pressed', 'false');
+    } else if (button.getAttribute('aria-pressed') === 'false') {
+      button.setAttribute('aria-pressed', 'true');
+    }
   });
 });
 
